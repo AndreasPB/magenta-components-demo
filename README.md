52e047c6da70dc2f9cec4cd96dc7b4f841113761
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-06-16 10:24:01 +0200

    Change default message to be about the project

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Demo components for the Svelte workshop
 
+Demo at https://magenta-components-demo.vercel.app/
+
 `npm install or yarn add magenta-components-demo`
 
 ```html
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Magenta Components Demo</h1>
+<p>
+	Visit <a href="https://www.npmjs.com/package/magenta-components-demo"
+		>npmjs.com/package/magenta-components-demo</a
+	> to read the documentation
+</p>
