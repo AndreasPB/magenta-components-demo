ad1e650de78504d9cca2f555ea6f353d54cbdec0
Andreas Brodersen <43907402+AndreasPB@users.noreply.github.com>
2022-06-16 09:38:52 +0200

    Add a nav and setup NPM published package

---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li><strong>Magenta</strong></li>
+	</ul>
+	<ul>
+		<li><a href="#">Link</a></li>
+		<li><a href="#">Link</a></li>
+		<li><a href="/" role="button">Home</a></li>
+	</ul>
+</nav>
